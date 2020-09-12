9196a4b434b8824020a6d8793f431ba9d0c1be96
StarUnderNight <1424249401@qq.com>
2020-09-12 13:45:32 +0800

    医疗项目版本V2，使用接口，使得程序具有可扩展性

---
+++ app/src/main/java/com/dkss/medicialv2/util/BufferQueue.java
@@ -0,0 +1,44 @@
+package com.dkss.medicialv2.util;
+
+import java.util.ArrayList;
+
+public class BufferQueue {
+
+    private ArrayList<byte[]> bufList = new ArrayList<>();
+    private int capacity = 10;
+
+    public BufferQueue(){
+
+    }
+
+    public BufferQueue(int capacity){
+        this.capacity = capacity;
+    }
+
+    public boolean add(byte[] data){
+        if(bufList.size()>= capacity){
+            bufList.remove(0);
+            bufList.add(data);
+            return false;
+        }
+        bufList.add(data);
+        return true;
+    }
+
+    public int size(){
+        return bufList.size();
+    }
+
+    public byte[] get(int index){
+        if(index>=bufList.size()){
+            return null;
+        }
+        return bufList.get(index);
+    }
+    public void remove(int index){
+        if(index>=bufList.size()){
+            return;
+        }
+        bufList.remove(index);
+    }
+}
